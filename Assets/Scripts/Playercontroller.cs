13dfd5c83a6ed4c17cc44474ef89d387435fa54a
BryanReynoso1 <827627@mystma.org>
2021-01-12 12:55:21 -0600

    Player jump
    Player jump

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Playercontroller : MonoBehaviour
 {
+
+    public float jumpPower;
+
+    public float moveSpeed;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpPower);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector3(moveSpeed, rb.velocity.y , 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y ,0);
+        }
+
 
     }
 }
