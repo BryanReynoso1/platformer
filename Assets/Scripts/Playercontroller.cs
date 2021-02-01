236b98144ce88a3711665c4adbc5ea5baee9dd5d
BryanReynoso1 <827627@mystma.org>
2021-02-01 13:33:04 -0600

    Single jump, Moving platforms, and camera controller
    Single jump, moving platforms, and camera controller

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -9,15 +9,22 @@ public class Playercontroller : MonoBehaviour
 
     public float moveSpeed;
 
+    public float jumpDetectOffset;
+
+    public LayerMask jumpableObject;
+
     Rigidbody2D rb;
 
     SpriteRenderer sr;
+    Vector3 startPos;
+    BoxCollider2D bc;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        bc = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -30,7 +37,7 @@ public class Playercontroller : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            rb.velocity = new Vector3(moveSpeed, rb.velocity.y , 0);
+            rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
             sr.flipX = true;
         }
 
@@ -44,21 +51,27 @@ public class Playercontroller : MonoBehaviour
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            rb.velocity = new Vector3(0, rb.velocity.y ,0);
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
             sr.flipX = false;
         }
 
 
     }
 
-
     private void OnCollisionEnter2D(Collision2D c)
     {
+        if (c.gameObject.tag == "Respawn")
+        {
+            rb.velocity = Vector3.zero;
+            transform.position = startPos;
+        }
         if (c.gameObject.tag == "Platform")
         {
             transform.SetParent(c.transform);
         }
     }
+
+
 
     private void OnCollisionExit2D(Collision2D c)
     {
@@ -67,4 +80,15 @@ public class Playercontroller : MonoBehaviour
             transform.SetParent(null);
         }
     }
-}
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0f, Vector2.down, jumpDetectOffset, jumpableObject);
+        Color raycolor = Color.red;
+            Debug.DrawRay(new Vector3(0, bc.bounds.extents.y + jumpDetectOffset), Vector2.right * bc.bounds.size.x, raycolor);
+            return raycastHit.collider != null;
+        }
+
+    }
+
+
